400bdb75bb920a653b5d2763daa99bf8a85d7481
amk09 <hanjue03@bu.edu>
2025-12-07 23:21:56 -0500

    follow-up details

---
+++ build/CMakeFiles/css_interactive.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/oscar/home/jhan192/CSS-Object-Recognition/bin/css_interactive"
+  "/oscar/home/jhan192/CSS-Object-Recognition/bin/css_interactive.pdb"
+  "CMakeFiles/css_interactive.dir/cmd/css_interactive.cpp.o"
+  "CMakeFiles/css_interactive.dir/cmd/css_interactive.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/css_interactive.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/css_interactive.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for css_interactive.
+# This may be replaced when dependencies are built.
